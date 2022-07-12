744278c944d95f96f7425d4a4d8f182fa9df24b9
tenwishes <hateglock@gmail.com>
2022-07-12 18:49:51 +0300

    Updates

---
+++ precache-manifest.1a8cc691813008046eb49a961b460d72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6039b433f6477d21275aa6db2a156b7b",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "cf9589037375137ea13d",
+    "url": "/todo-list/static/css/main.218a5c34.chunk.css"
+  },
+  {
+    "revision": "ba6f9d2fcfda00a49e16",
+    "url": "/todo-list/static/js/2.bc596599.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo-list/static/js/2.bc596599.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf9589037375137ea13d",
+    "url": "/todo-list/static/js/main.afe89927.chunk.js"
+  },
+  {
+    "revision": "f5ee74d33c5afdf3bdbc",
+    "url": "/todo-list/static/js/runtime-main.bd857350.js"
+  }
+]);
